86f21292320a8846b8e44ca286bc9adaab0f61eb
Rodney Pilgrim <rodneyp290@gmail.com>
2017-06-29 00:06:54 +1000

    ENHANCE: Improve hanoi4, although still work in progress as not optimal for 15 disc on 4 pegs

--- fp2/session01/TowersOfHanoi-extended.hs
+++ fp2/session01/TowersOfHanoi-extended.hs
@@ -14,9 +14,14 @@ module TowersOfHanoiExtended where
    | otherwise = (hanoi (n-1) src tmp tgt) ++ ((src, tgt):(hanoi (n-1) tmp tgt src))
  
  hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
- hanoi4 1 src tgt tmp1 tmp2 = [(src, tgt)]
- hanoi4 n src tgt tmp1 tmp2 = 
-   (hanoi4 (n-1) src tmp1 tmp2 tgt) ++ ((src, tgt):(hanoi4 (n-1) tmp2 tmp1 tgt src))
+ hanoi4 n src tmp1 tmp2 tgt
+   | n <  1 = []
+   | n == 1 = [(src, tgt)]
+   | n == 2 = [(src, tmp1),(src, tgt),(tmp1,tgt)]
+   | otherwise =
+              (hanoi4 (n-2) src tgt tmp1 tmp2)
+           ++ ( (src, tmp1) : (src, tgt) : (tmp1,tgt)
+            : (hanoi4 (n-2) tmp2 src tmp1 tgt) )
 
  moves  = (hanoi 4 "a" "b" "c") 
  moves4 = (hanoi4 4 "a" "b" "c" "d") 
