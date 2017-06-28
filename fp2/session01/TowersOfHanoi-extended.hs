7bf5fad164d533640065a91fe4579df7599dfa2d
Rodney Pilgrim <rodneyp290@gmail.com>
2017-06-29 00:14:27 +1000

    QUALITY: Make hanoi4 fit specs implied pattern of hanoi <source peg> <target peg> <temporary peg(s)>

--- fp2/session01/TowersOfHanoi-extended.hs
+++ fp2/session01/TowersOfHanoi-extended.hs
@@ -14,14 +14,14 @@ module TowersOfHanoiExtended where
    | otherwise = (hanoi (n-1) src tmp tgt) ++ ((src, tgt):(hanoi (n-1) tmp tgt src))
  
  hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
- hanoi4 n src tmp1 tmp2 tgt
+ hanoi4 n src tgt tmp1 tmp2
    | n <  1 = []
    | n == 1 = [(src, tgt)]
    | n == 2 = [(src, tmp1),(src, tgt),(tmp1,tgt)]
    | otherwise =
-              (hanoi4 (n-2) src tgt tmp1 tmp2)
+              (hanoi4 (n-2) src tmp2 tgt tmp1)
            ++ ( (src, tmp1) : (src, tgt) : (tmp1,tgt)
-            : (hanoi4 (n-2) tmp2 src tmp1 tgt) )
+            : (hanoi4 (n-2) tmp2 tgt src tmp1) )
 
  moves  = (hanoi 4 "a" "b" "c") 
  moves4 = (hanoi4 4 "a" "b" "c" "d") 
