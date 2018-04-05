dc302c09067d0138e894e63d50030116a3b0ff48
Rodney Pilgrim <rodneyp290@gmail.com>
2018-04-05 17:42:54 +1000

    NEW: Lecture 12 - Exercise 2

--- fp2/lecture12/Risk.hs
+++ fp2/lecture12/Risk.hs
@@ -4,6 +4,10 @@ module Risk where
 
 import Control.Monad.Random
 
+
+import Data.List -- Needed for `sort`
+import Data.Bool -- Needed for `bool`
+
 ------------------------------------------------------------
 -- Die values
 
@@ -26,3 +30,34 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+  deriving Show
+
+
+------------------------------------------------------------
+-- Exercise 2 - battle :: Battlefield -> Rand StdGen Battlefield
+-- (Exercise 1 was installing MonadRandom)
+
+-- helper function for shorthand Battlefield Constructor
+bf = Battlefield
+
+battle :: Battlefield -> Rand StdGen Battlefield
+--battle b@(Battlefield a d) = outcome <$> pure b <*> pure [(DV 5),(DV 3)] <*> pure[(DV 4),(DV 3)]
+battle b@(Battlefield a d) = outcome <$> pure b <*> rollNDie an <*> rollNDie dn
+  where
+    an = min 3 (a-1)
+    dn = min 2 d
+
+rollNDie :: Int -> Rand StdGen [DieValue]
+rollNDie 0 = pure []
+rollNDie n = (:) <$> die <*> (rollNDie (n-1))
+
+outcome :: Battlefield -> [DieValue] -> [DieValue] -> Battlefield
+outcome b as ds = outcome' b (sortOn negate as) (sortOn negate ds)
+  where
+    outcome' b' _ [] = b'
+    outcome' b' [] _ = b'
+    outcome' (Battlefield a d) (ad:ads) (dd:dds) =
+      outcome' (bf (a-aloss) (d-dloss)) ads dds
+      where
+        aloss = bool (1)(0) ((unDV ad) > (unDV dd))
+        dloss = 1 - aloss --bool (0)(1) ((unDV ad) > (unDV dd))
